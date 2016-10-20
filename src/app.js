20b428d46da9392bf27c19b79b241dc7521aa9ba
Justin Hugg <justin.hugg@gmail.com>
2016-10-19 22:01:22 -0400

    setup for code retreat

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/* jshint node: true */
+'use strict';
+
+let Me = require("./classes/myclass.js");
+
+let me = new Me('Justin');
+me.getName();
---
+++ src/classes/myclass.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let test = class MyClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    console.log(`My name is ${this.name}.`);
+  }
+};
+
+module.exports = test;
